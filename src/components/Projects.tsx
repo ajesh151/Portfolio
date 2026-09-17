88687cea5220ce5564d8abea4e97b99ad202cc65
ajesh151 <ajesh4325@student.ku.edu.np>
2026-09-17 19:02:06 +0545

    image modified

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -31,7 +31,7 @@ export function Projects() {
                     width={1024}
                     height={640}
                     loading="lazy"
-                    className="aspect-[16/10] w-full object-contain transition-transform duration-500 group-hover:scale-[1.02]"
+                    className="aspect-[16/9] w-full object-cover transition-transform duration-500 group-hover:scale-[1.02]"
                   />
                 </button>
                 <div className="flex flex-1 flex-col p-6">
